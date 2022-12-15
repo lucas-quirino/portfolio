2009b4bb760435a690714e970f74cae406447610
lucas-quirino <luksquirino@edu.unirio.br>
2022-12-15 03:33:49 -0300

    criando repositório

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// You can use a ScrollTrigger in a tween or timeline
+gsap.to("svg", {
+  y: 15,
+  scrollTrigger: {
+    trigger: "path",
+    start: "500px 480px",
+    end: "300px 100px",
+    scrub: true,
+    // markers: true,
+    id: "scrub",
+  }
+});
+
+
+
+
+gsap.set(".ball", {xPercent: -50, yPercent: -50});
+
+const ball = document.querySelector(".ball");
+const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+const mouse = { x: pos.x, y: pos.y };
+const speed = 0.2;
+
+const xSet = gsap.quickSetter(ball, "x", "px");
+const ySet = gsap.quickSetter(ball, "y", "px");
+
+window.addEventListener("mousemove", e => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+});
+
+gsap.ticker.add(() => {
+
+  // adjust speed for higher refresh monitors
+  const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
+
+  pos.x += (mouse.x - pos.x) * dt;
+  pos.y += (mouse.y - pos.y) * dt;
+  xSet(pos.x);
+  ySet(pos.y);
+});
